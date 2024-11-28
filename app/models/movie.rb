f8de3bc137a000012abbe3214161d405708734fd
jerome balen <jerome.balen@yahoo.fr>
2024-11-28 14:48:45 +0100

    update

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,15 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :restrict_with_error
+  has_many :bookmarks
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
+
+  private
+
+  # Méthode pour vérifier la présence de Bookmarks avant de détruire un Movie
+
+  def check_for_bookmarks
+    if bookmarks.exists?
+      raise ActiveRecord::InvalidForeignKey, "Cannot destroy movie with associated bookmarks"
+    end
+  end
 end
