d5d770976a98cb8191da1127d2d2460284a74a42
jerome balen <jerome.balen@yahoo.fr>
2024-11-28 12:15:29 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_28_105513) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_28_111457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_105513) do
     t.text "overview"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "poster_url"
+    t.decimal "rating"
   end
 
   add_foreign_key "bookmarks", "lists"
